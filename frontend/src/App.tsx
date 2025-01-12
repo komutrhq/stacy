f98b5c30985aaf44e2cdbca831bd04d56a2eb6e6
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2025-01-12 20:56:10 +0700

    Update frontend/src/App.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { HelmetProvider } from "react-helmet-async";
 
 function App() {
 	return (
-		<HelmetProvider context={{}}>
+		<HelmetProvider>
 			<h1 className="text-3xl font-bold font-display">Hello world!</h1>
 		</HelmetProvider>
 	);
